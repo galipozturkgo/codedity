3b4ccc7546b8be838ba65ad5e81150d3e5833404
Galip Ozturk <galipozturk.go@gmail.com>
2022-10-10 18:08:13 +0300

    Build to preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as esbuild from "esbuild-wasm";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const iframe = useRef<HTMLIFrameElement | null>(null);
 
   useEffect(() => { startService(); }, [])
 
@@ -17,6 +17,10 @@ const App = () => {
   }
 
   const onClick = async () => {
+    if (!iframe.current?.contentWindow) return null;
+
+    iframe.current.srcdoc = html;
+
     const res = await esbuild.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -30,11 +34,36 @@ const App = () => {
       },
     });
 
-    setCode(res.outputFiles[0].text)
+    iframe.current.contentWindow.postMessage(res.outputFiles[0].text, "*");
   }
+
+
+  const html = `
+  <html>
+    <head>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => { 
+          try {
+            eval(event.data); 
+          }
+          catch (err) {
+            const root = document.getElementById('root');
+            root.innerHTML = '<div style="color : red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
+`
 
   return <div>
     <textarea
+      rows={10}
+      cols={40}
       value={input}
       onChange={(e) => setInput(e.target.value)}
     />
@@ -43,7 +72,7 @@ const App = () => {
         Submit
       </button>
     </div>
-    <pre>{code}</pre>
+    <iframe ref={iframe} title="codepreview" sandbox="allow-scripts" srcDoc={html} />
   </div>;
 }
 
